867e6fb38dcc023e9663e51225f78892cf3c1044
pejanov <pejanovjovan@gmail.com>
2025-11-11 18:50:39 +0100

    Update info.js

--- commands/info.js
+++ commands/info.js
@@ -11,7 +11,7 @@ module.exports = {
 			.setDescription('Here are some useful details!')
 			.addFields(
 				{ name: 'Server IP', value: '`play.pika-network.net`', inline: true },
-				{ name: 'Developer', value: '`muurt`', inline: true },
+				{ name: 'Developer', value: '`xhome`', inline: true },
 				{ name: 'Bot Version', value: '`1.1`', inline: true }
 			)
 			.setTimestamp();
