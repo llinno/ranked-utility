6b8f47ca33e3171002df1966290cee05c7d2c4b6
pejanov <pejanovjovan@gmail.com>
2025-11-11 18:46:26 +0100

    Update info.js

--- commands/info.js
+++ commands/info.js
@@ -10,8 +10,8 @@ module.exports = {
 			.setTitle('Server Information')
 			.setDescription('Here are some useful details!')
 			.addFields(
-				{ name: 'Server IP', value: '`zrbw.fun`', inline: true },
-				{ name: 'Developer', value: '`leesyntax`', inline: true },
+				{ name: 'Server IP', value: '`play.pika-network.net`', inline: true },
+				{ name: 'Developer', value: '`muurt`', inline: true },
 				{ name: 'Bot Version', value: '`1.1`', inline: true }
 			)
 			.setTimestamp();
